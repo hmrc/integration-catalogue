9659b2ccd36c2fe99e8c0bb625ad8bb18729a946
anjumabbas <khaakpaayali@gmail.com>
2022-11-30 15:42:31 +0000

    API-5564 - Updated bootstrap version to 7.12, sbt plugin to 2.8.18

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,17 @@ object AppDependencies {
 
   lazy val scalaCheckVersion = "1.14.0"
   lazy val enumeratumVersion = "1.6.3"
-  lazy val hmrcmongoVersion = "0.72.0"
-  lazy val bootstrapVersion = "5.24.0"
-  lazy val jacksonVersion = "2.11.1"
+  lazy val hmrcMongoVersion = "0.74.0"
+  lazy val bootstrapVersion = "7.12.0"
+  lazy val jacksonVersion = "2.12.2"
 
   val compile = Seq(
     "uk.gov.hmrc"                       %% "bootstrap-backend-play-28"              % bootstrapVersion,
     "com.typesafe.play"                 %% "play-json"                              % "2.9.2",
     "com.typesafe.play"                 %% "play-json-joda"                         % "2.9.2",
     "com.beachape"                      %% "enumeratum-play-json"                   % enumeratumVersion,
-    "uk.gov.hmrc.mongo"                 %% "hmrc-mongo-play-28"                     % hmrcmongoVersion,
-    "uk.gov.hmrc.mongo"                 %% "hmrc-mongo-work-item-repo-play-28"      % hmrcmongoVersion,
+    "uk.gov.hmrc.mongo"                 %% "hmrc-mongo-play-28"                     % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo"                 %% "hmrc-mongo-work-item-repo-play-28"      % hmrcMongoVersion,
 
     "com.fasterxml.jackson.module"      %% "jackson-module-scala"           % jacksonVersion,
     "com.fasterxml.jackson.core"        % "jackson-annotations"             % jacksonVersion,
@@ -31,7 +31,7 @@ object AppDependencies {
     "org.pegdown"             %  "pegdown"                  % "1.6.0"             % "test, it",
     "org.mockito"             %% "mockito-scala-scalatest"  % "1.7.1"           % "test, it",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % hmrcmongoVersion    % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion    % "test, it",
     "com.github.tomakehurst"  % "wiremock-jre8-standalone"  % "2.27.1"            % "test, it",
     "org.scalacheck"          %% "scalacheck"               % scalaCheckVersion   % "test, it"
   )
